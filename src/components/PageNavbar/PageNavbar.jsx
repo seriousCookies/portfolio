12768d7b5cb1cc7a35517932285938cd5a652669
Na Wei <na.wei95@hotmail.com>
2021-03-29 23:43:21 +0200

    navbar update

--- src/components/PageNavbar/PageNavbar.jsx
+++ src/components/PageNavbar/PageNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav, Container, Row } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 
 import Fade from 'react-reveal/Fade';
